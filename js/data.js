8e53cb65c34d2f319e648f5698632d674dc3dd6d
WoosterUK <WoosterUK@users.noreply.github.com>
2021-10-26 12:58:36 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,66 @@
+//const MINERALS = ["Tritanium","Pyerite","Mexallon","Isogen","Nocxium","Zydrine","Megacyte","Morphite"];
+const REFINE = [
+  {
+    "input": "Veldspar",
+    "output": [{"mineral": "Tritanium", "quantity":400}]
+  },
+  {
+    "input": "Scordite",
+    "output": [{"mineral": "Tritanium", "quantity": 150}, {"mineral": "Pyerite", "quantity": 90}]
+  },
+  {
+    "input": "Pyroxeres",
+    "output": [{"mineral": "Pyerite", "quantity": 90}, {"mineral": "Mexallon", "quantity": 30}]
+  },
+  {
+    "input": "Plagioclase",
+    "output": [{"mineral": "Tritanium", "quantity": 175}, {"mineral": "Mexallon", "quantity": 70}]
+  },
+  {
+    "input": "Omber",
+    "output": [{"mineral": "Pyerite", "quantity": 90}, {"mineral": "Isogen", "quantity": 75}]
+  },
+  {
+    "input": "Kernite",
+    "output": [{"mineral": "Mexallon", "quantity": 60}, {"mineral": "Isogen", "quantity": 120}]
+  },
+  {
+    "input": "Jaspet",
+    "output": [{"mineral": "Mexallon", "quantity": 60}, {"mineral": "Nocxium", "quantity": 50}]
+  },
+  {
+    "input": "Hemorphite",
+    "output": [{"mineral": "Isogen", "quantity": 240}, {"mineral": "Nocxium", "quantity": 90}]
+  },
+  {
+    "input": "Hedbergite",
+    "output": [{"mineral": "Pyerite", "quantity": 450}, {"mineral": "Nocxium", "quantity": 120}]
+  },
+  {
+    "input": "Gneiss",
+    "output": [{"mineral": "Pyerite", "quantity": 2000}, {"mineral": "Mexallon", "quantity": 1500}, {"mineral": "Isogen", "quantity": 800}]
+  },
+  {
+    "input": "Dark Ochre",
+    "output": [{"mineral": "Mexallon", "quantity": 1360}, {"mineral": "Isogen", "quantity": 1200}, {"mineral": "Nocxium", "quantity": 320}]
+  },
+  {
+    "input": "Crokite",
+    "output": [{"mineral": "Pyerite", "quantity": 800}, {"mineral": "Mexallon", "quantity": 2000}, {"mineral": "Nocxium", "quantity": 800}]
+  },
+  {
+    "input": "Spodumain",
+    "output": [{"mineral": "Tritanium", "quantity": 48000}, {"mineral": "Isogen", "quantity": 1000}, {"mineral": "Nocxium", "quantity": 160}, {"mineral": "Zydrine", "quantity": 80}, {"mineral": "Megacyte", "quantity": 40}]
+  },
+  {
+    "input": "Bistot",
+    "output": [{"mineral": "Pyerite", "quantity": 3200}, {"mineral": "Mexallon", "quantity": 1200}, {"mineral": "Zydrine", "quantity": 160}]
+  },
+  {
+    "input": "Arkonor",
+    "output": [{"mineral": "Pyerite", "quantity": 3200}, {"mineral": "Mexallon", "quantity": 1200}, {"mineral": "Megacyte", "quantity": 120}]
+  },
+  {
+    "input": "Mercoxit",
+    "output": [{"mineral": "Morphite", "quantity": 140}]
+  }];
